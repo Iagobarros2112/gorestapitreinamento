1ce2b364783d3e852baa3214a216d7d6d31eae86
Iagobarros2112 <iagobarrosgomes2112@gmail.com>
2024-11-01 09:31:54 -0300

    yjsthar continuaçao do treinamento

--- cmd/main.go
+++ cmd/main.go
@@ -1,17 +1,38 @@
 package main
 
 import (
+	"go-api/controller"
+	"go-api/db"
+	"go-api/repository"
+	"go-api/usecase"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	server := gin.Default()
 
+	dbConnection, err := db.ConnectDB()
+
+	if err != nil {
+		panic(err)
+	}
+	//repositories
+	ProductRepository := repository.NewProductRepository(dbConnection)
+
+	//usecases
+	ProductUseCase := usecase.NewProductUseCase(ProductRepository)
+
+	//controllers
+	productController := controller.NewProductController(ProductUseCase)
+
 	server.GET("/ping", func(ctx *gin.Context) {
 		ctx.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
+
+	server.GET("/products", productController.GetProducts)
 
 	server.Run(":8000")
 
---
+++ controller/product_controller.go
@@ -0,0 +1,28 @@
+package controller
+
+import (
+	"go-api/usecase"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type productController struct {
+	ProductUsecase usecase.ProductUsecase
+}
+
+func NewProductController(usecase usecase.ProductUsecase) productController {
+	return productController{
+		ProductUsecase: usecase,
+	}
+}
+
+func (p *productController) GetProducts(ctx *gin.Context) {
+	products, err := p.ProductUsecase.GetProducts()
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, err)
+	}
+
+	ctx.JSON(http.StatusOK, products)
+
+}
